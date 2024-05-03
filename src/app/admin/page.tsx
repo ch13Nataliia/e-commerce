4c9cd301ffa4771c8a3eac5687aaa072c4b4f8df
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-05-03 11:52:41 +0100

    added card from shadcn and created single card

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,43 @@
-export default function AdminDashboard() {
-  return <div>
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
-  </div>
+
+
+export default function AdminDashboard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle="Subtitle"
+        body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laborum est maxime accusantium. Ab ipsum ipsam vero unde hic mollitia, maiores, distinctio facilis earum ducimus eius a temporibus fuga suscipit."
+      />
+      
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,10 +16,11 @@ export function NavLink(props: Omit<ComponentProps<typeof Link>, 'className'>) {
   const pathname = usePathname();
   return (
     <Link
-      {...props}
-      className={cn(
-        'p-4 hover:bg-secondary hover:text-secondary-foreground  focus-visible:bg-secondary focus-visible:text-secondary-foreground', pathname === props.href && "bg-background text-background"
-      )}
-    />
+    {...props}
+    className={cn(
+      "p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
+      pathname === props.href && "bg-background text-foreground"
+    )}
+  />
   );
 }
